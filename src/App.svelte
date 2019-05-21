7e6e79c7eb9937674758cb7839be673637523bcf
Zach Morse <zacharymorse@gmail.com>
2019-05-20 22:03:49 -0600

    project init

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  .tr {
+    width: 100%;
+    margin: auto;
+    height: 100%;
+    position: relative;
+    /* border: 3px solid saddlebrown; */
+    overflow: hidden;
+  }
+
+  .tr:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 20%;
+    width: 100%;
+    height: 100vh;
+    background: #0079c6;
+    transform-origin: 0 100%;
+    transform: rotate(45deg);
+  }
+</style>
+
+<div class="tr">
+  <div class="interior" />
+</div>
